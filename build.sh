f106836e6c9c974f9676e4c8bceca95d2eebb701
KK <bebound@gmail.com>
2021-12-11 16:29:18 +0800

    Minor update

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# docker buildx install
-# PLATFORMS=linux/arm64,linux/amd64,linux/riscv64,linux/ppc64le,linux/s390x,linux/386,linux/arm/v7,linux/arm/v6
-# docker build . -t caddy_builder --platform $PLATFORMS  -f Dockerfile_builder
-docker build . -t caddy_builder  -f Dockerfile_builder
+PLATFORMS=linux/arm64,linux/386,linux/arm64,linux/arm/v7
+docker build . -t caddy_builder --platform $PLATFORMS  -f Dockerfile_builder
 
 name='dnspod'
 repo=github.com/caddy-dns/$name
@@ -19,9 +17,7 @@ echo Current valid version is $final_tag
 docker manifest inspect $final_tag &> /dev/null
 if [ $? -ne 0 ]; then
   echo Building $final_tag
-  docker build . -t $final_tag -t $final_tag_no_version --platform $PLATFORMS -f Dockerfile --build-arg DNS=$repo
-  docker push $final_tag
-  docker push $final_tag_no_version
+  docker build . -t $final_tag -t $final_tag_no_version --platform $PLATFORMS -f Dockerfile --build-arg DNS=$repo --push
 else
   echo "Already exists, skip build"
 fi
