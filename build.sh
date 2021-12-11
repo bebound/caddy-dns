21771d226f7636d1b93f47fd0656bed2f54bb048
KK <bebound@gmail.com>
2021-12-11 23:42:12 +0800

    Minor fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker build . -t bebound/caddy_builder --platform $PLATFORMS  -f Dockerfile_bui
 build_with_dns(){
     name=$1
     repo=github.com/caddy-dns/$name
-    caddy_version=$(docker run -it --rm bebound/caddy_builder ./caddy version | cut -d ' ' -f 1)
+    caddy_version=$(docker run -it --rm bebound/caddy_builder ./caddy version | cut -d ' ' -f 1 | | xargs)
     if [ ! -d $name ]; then
         git clone https://$repo
     fi
